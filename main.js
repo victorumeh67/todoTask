220125df5d653949e8a1d9169bd94095499e6db8
victorumeh67 <victorumeh255@gmail.com>
2022-03-17 11:29:04 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', ()=>{
+    const form = document.querySelector('#new-task-form')
+    const input = document.querySelector('#new-task-input')
+    const list_el = document.querySelector('#tasks')
+
+    const task = input.value
+    if(task == ""){
+        alert(hello)
+    }
+
+})
+
+
